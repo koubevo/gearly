f06fa12f1ea67edb39a424a87250322d90ff804a
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-02 23:55:56 +0100

    add form

--- resources/js/Pages/Offer/Create.vue
+++ resources/js/Pages/Offer/Create.vue
@@ -1,9 +1,75 @@
 <template>
-    <form>
-        
+    <form @submit.prevent="create">
+        <input type="text" placeholder="Name" name="name" v-model="form.name" />
+        <!-- TODO: pridat datove typy v-model.number atd -->
+        <div>
+            <label class="cursor-pointer">
+                <input type="radio" name="phone" value="1" v-model="form.phone" />
+                <span>yes</span>
+            </label>
+            <label class="cursor-pointer">
+                <input type="radio" name="phone" value="0" v-model="form.phone" />
+                <span>no</span>
+            </label>
+        </div>
+
+        <textarea placeholder="Description" name="description" v-model="form.description"></textarea>
+
+        <input type="number" placeholder="Price" v-model="form.price" />
+
+        <select name="currency" v-model="form.currency">
+            <option value="CZK">CZK</option>
+            <option value="EUR">EUR</option>
+        </select>
+
+        <select name="condition" v-model="form.condition">
+            <option value="new">NEW</option>
+            <option value="used">USED</option>
+        </select>
+
+        <div>
+            <label class="cursor-pointer">
+                <input type="radio" name="sport" value="1" v-model="form.sport" />
+                <span>all</span>
+            </label>
+            <label class="cursor-pointer">
+                <input type="radio" name="sport" value="2" v-model="form.sport" />
+                <span>baseball</span>
+            </label>
+            <label class="cursor-pointer">
+                <input type="radio" name="sport" value="3" v-model="form.sport" />
+                <span>softball</span>
+            </label>
+        </div>
+
+        <select name="category" v-model="form.category">
+            <option value="1">Bats</option>
+            <option value="2">Gloves</option>
+        </select>
+
+        <select name="brand" v-model="form.brand">
+            <option value="1">Nike</option>
+            <option value="2">Rawlings</option>
+        </select>
+
+        <input type="submit" value="Add offer" />
     </form>
 </template>
 
 <script setup>
+import { useForm } from '@inertiajs/vue3' 
 
+const form = useForm({
+    name: "",
+    phone: "",
+    description: "",
+    price: null,
+    currency: "CZK",
+    condition: "new",
+    sport: "",
+    category: "",
+    brand: "",
+});
+
+const create = () => form.post('listing/');
 </script>
