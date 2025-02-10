f2a48e11e54b3645b7fb7425b128a9f3fadf972c
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-10 21:17:42 +0100

    move grid to OffersGrid.vue, add offer values to Card.vue

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -1,22 +1,36 @@
 <template>
-    <Link>
+    <Link :href="route('offer.show', offer.id)">
         <div class="bg-white p-2 shadow-gray-300 shadow-lg">
             <div class="relative">
-                <span class="absolute top-1 right-1 py-1 flex align-middle">
-                    <span class="bg-primary-900 h-full px-2 uppercase font-medium inline-block">nové</span>
-                    <button class="bg-white px-2"><HeartIcon class="w-5 h-5 mt-0.5" /></button>
+                <span class="absolute top-1 right-1 py-1 flex align-middle gap-2">
+                    <!-- TODO: bg depending on condition -->
+                    <span class="bg-primary-900 h-full uppercase font-medium inline-block px-2">{{ offer.condition }}</span>
+                    <!-- TODO: adding to wishlist, background -->
+                    <button><HeartIcon class="w-5 h-5 mt-0.5" /></button>
                 </span>
-            
-                <img :src="'storage/imgs/nike.jpg'" alt="" class="mb-2">
+                <!-- TODO: scaled img -->
+                <img :src="'storage/imgs/nike.jpg'" :alt="offer.name" class="mb-2">
             </div>
-            <p class="text-xs text-gray-700">Nike</p>
-            <h3 class="font-medium mb-2">Alpha Huarache NXT Tim Anderson PE HF5576-001</h3>
-            <p class="font-black">2150 CZK</p>
+            <!-- TODO: brand -->
+            <p class="text-xs text-gray-700">{{ offer.brand }}</p>
+            <h3 class="font-medium mb-2">{{ offer.name }}</h3>
+            <p class="font-black uppercase">{{ price }} {{ offer.currency }}</p>
         </div>
     </Link>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
 import { HeartIcon } from '@heroicons/vue/24/outline'
+
+const props = defineProps({
+  offer: Object
+});
+
+const price = computed(() => {
+    if (!props.offer || props.offer.price == null) return "0"; // Ochrana proti undefined/null
+    const numericPrice = Number(props.offer.price); // Převod na číslo
+    return numericPrice % 1 === 0 ? numericPrice.toFixed(0) : numericPrice.toFixed(2);
+});
 </script>
--- resources/js/Pages/Offer/Index.vue
+++ resources/js/Pages/Offer/Index.vue
@@ -1,15 +1,11 @@
 <template>
-    <!-- TODO: move to OffersGrid.vue -->
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8">
-        <Card v-for="offer in offers" :key="offer.id">
-        </Card>
-    </div>
+    <OffersGrid :offers="offers"/>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
     import MainLayout from '@/Layouts/MainLayout.vue';
-    import Card from '@/Components/Card.vue';
+    import OffersGrid from '@/Components/OffersGrid.vue';
 
     defineOptions({ layout: MainLayout })
 
