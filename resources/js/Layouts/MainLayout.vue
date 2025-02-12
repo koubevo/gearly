5340030e8ca96dc7e2316a61796d83897deeba44
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-12 22:08:36 +0100

    edit login page styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="border-b-2 border-black border-solid w-full mb-2">
+    <header class="border-b-2 border-black border-solid w-full">
         <div class="container mx-auto">
             <nav class="py-3 max-md:px-2 flex items-center justify-between">
                 <div class="flex gap-3">
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -31,69 +31,41 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Log in" />
-
+        <!-- TODO: whats this -->
         <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
             {{ status }}
         </div>
 
+        <!-- TODO: add autocomplete -->
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
+                <input type="email" placeholder="Email" name="email" v-model="form.email" class="input-style" required />
+                <div v-if="form.errors.email" class="input-error-message-style">{{ form.errors.email }}</div>
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
+                <input type="password" placeholder="Password" name="password" v-model="form.password" class="input-style" required />
+                <div v-if="form.errors.password" class="input-error-message-style">{{ form.errors.password }}</div>
             </div>
 
             <div class="mt-4 block">
                 <label class="flex items-center">
                     <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600"
-                        >Remember me</span
-                    >
+                    <span class="ms-2 text-sm">Remember me</span>
                 </label>
             </div>
 
-            <div class="mt-4 flex items-center justify-end">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
+            <div class="mt-4 flex">
+                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
+            </div>
+
+            <div class="mt-4">
+                <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="rounded-md text-sm underline hover:text-black focus:outline-none">
+                    Forgot your password?
+                </Link>
             </div>
         </form>
     </GuestLayout>
