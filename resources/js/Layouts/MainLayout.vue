fec84266541c48a97f494aa6e64825f030495359
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-13 15:11:06 +0100

    edit mainlayout, add plus icon, add animation

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -11,14 +11,34 @@
                     </Link>
                 </div>
                 <div class="flex gap-2" v-if="user">
-                    <Link :href="route('landingPage')">
-                        <BellIcon class="w-5 h-5 mt-0.5" />
+                    <Link :href="route('offer.create')">
+                        <div class="relative w-6 h-6 mt-0.5">
+                            <svg class="w-full h-full stroke-[3]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <defs>
+                                    <linearGradient id="animatedGradient" x1="-100%" y1="0%" x2="200%" y2="0%">
+                                        <stop offset="0%" stop-color="#1D9E1D">
+                                            <animate attributeName="offset" values="-1;2" keyTimes="0;1" dur="10s" repeatCount="indefinite"/>
+                                        </stop>
+                                        <stop offset="50%" stop-color="black">
+                                            <animate attributeName="offset" values="-0.5;2.5" keyTimes="0;1" dur="10s" repeatCount="indefinite"/>
+                                        </stop>
+                                        <stop offset="100%" stop-color="#1D9E1D">
+                                            <animate attributeName="offset" values="0;3" keyTimes="0;1" dur="10s" repeatCount="indefinite"/>
+                                        </stop>
+                                    </linearGradient>
+                                </defs>
+                                <path stroke="url(#animatedGradient)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M12 4v16m-8-8h16"/>
+                            </svg>
+                        </div>
                     </Link>
                     <Link :href="route('landingPage')">
-                        <HeartIcon class="w-5 h-5 mt-0.5" />
+                        <BellIcon class="w-6 h-6 mt-0.5" />
+                    </Link>
+                    <Link :href="route('landingPage')">
+                        <HeartIcon class="w-6 h-6 mt-0.5" />
                     </Link>
                     <Link :href="route('profile.edit')">
-                        <UserIcon class="w-5 h-5 mt-0.5" />   
+                        <UserIcon class="w-6 h-6 mt-0.5" />   
                     </Link>
                 </div>
                 <div class="flex gap-2 mt-0.5" v-else>
@@ -30,7 +50,6 @@
     </header>
 
     <main class="container mx-auto max-md:px-2">
-        <!-- TODO: Component and better styling -->
         <div v-if="flashSuccess" class="mb-4 shadow-sm border-s-primary-900 border-4 border-e-0 border-y-0 p-2 bg-primary-500">
             {{ flashSuccess }}
         </div>
@@ -41,13 +60,11 @@
 <script setup>
 import { Link, usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
-import { MagnifyingGlassIcon, BellIcon, UserIcon, HeartIcon } from '@heroicons/vue/24/outline'
+import { MagnifyingGlassIcon, BellIcon, UserIcon, HeartIcon } from '@heroicons/vue/24/outline';
 import SecondaryLink from '@/Components/SecondaryLink.vue';
 import PrimaryLink from '@/Components/PrimaryLink.vue';
 
-
 const user = computed(() => page.props.auth.user);
 const page = usePage();
-
 const flashSuccess = computed(() => page.props.flash?.success ?? '');
 </script>
