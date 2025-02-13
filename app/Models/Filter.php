11032e094ca0c2c2a8f4e03b8d0413a60c977b6f
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-13 17:08:04 +0100

    add relation between filter and filterCategory model

--- app/Models/Filter.php
+++ app/Models/Filter.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Filter extends Model
 {
-    //
+    public function filterCategory(): BelongsTo
+    {
+        return $this->belongsTo(FilterCategory::class, 'filter_category_id');
+    }
 }
--- app/Models/FilterCategory.php
+++ app/Models/FilterCategory.php
@@ -3,11 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FilterCategory extends Model
 {
-    public function categories()
+    public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, 'filter_fc_mappings');
+    }
+
+    public function filters(): HasMany
+    {
+        return $this->hasMany(Offer::class, 'filter_category_id');
     }
 }
