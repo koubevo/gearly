331d0f8d36c18976e66bba786d54f2faf554d8a3
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-13 19:11:17 +0100

    change flash message style

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -50,9 +50,13 @@
     </header>
 
     <main class="container mx-auto max-md:px-2">
-        <div v-if="flashSuccess" class="mb-4 shadow-sm border-s-primary-900 border-4 border-e-0 border-y-0 p-2 bg-primary-500">
+        <!-- TODO: error message, better styling -->
+        <div v-if="flashSuccess" 
+             class="my-4 shadow-sm border-s-primary-900 border-4 border-e-0 border-y-0 p-2 
+                    bg-gradient-to-r from-primary-500 via-white to-white w-1/2">
             {{ flashSuccess }}
         </div>
+
         <slot></slot>
     </main>   
 </template>
