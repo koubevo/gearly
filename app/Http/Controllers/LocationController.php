9f14fddd61b29e5360df46df8c3c106baad76cfd
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-16 15:04:01 +0100

    feat: add LocationController with methods to fetch countries and cities; update API routes

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Nnjeim\World\World;
 
 class LocationController extends Controller
 {
-    //
+    public function getCountries()
+    {
+        $allowedCountries = ['CZ', 'NL', 'IT', 'DK', 'DE', 'ES', 'AT', 'BE', 'PL', 'HR', 'FR', 'SK', 'SE', 'GB'];
+
+        $countries = World::countries([
+            'fields' => 'iso2,name',
+            'filters' => [
+                'region' => 'Europe'
+            ]
+        ])->data;
+
+        $filteredCountries = $countries->filter(fn($country) => in_array($country['iso2'], $allowedCountries));
+
+        // sort by popularity
+        $sortedCountries = $filteredCountries->sortBy(fn($country) => array_search($country['iso2'], $allowedCountries));
+
+        return response()->json([
+            'success' => true,
+            'data' => $sortedCountries->values(), // Resetuje indexy
+        ]);
+    }
+
+
+
+    public function getCities(Request $request)
+    {
+        $request->validate([
+            'country_id' => 'required|integer'
+        ]);
+
+        $cities = World::cities()
+            ->where('country_id', $request->country_id)
+            ->orderBy('name')
+            ->get();
+
+        return response()->json($cities);
+    }
 }
--- resources/js/Components/OfferForm.vue
+++ resources/js/Components/OfferForm.vue
@@ -210,7 +210,7 @@ const fetchFilterOptions = async (categoryId) => {
         const selectedCategory = props.categories.find(cat => cat.id === categoryId);
         if (selectedCategory) {
             const responses = await Promise.all(
-                selectedCategory.filters.map(filter => axios.get(`http://127.0.0.1:8000/filters/${filter.id}`))
+                selectedCategory.filters.map(filter => axios.get(`http://127.0.0.1:8000/api/filters/${filter.id}`))
             );
             filteredFilterCategories.value = selectedCategory.filters.map((filter, index) => ({
                 ...filter,
