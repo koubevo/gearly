7218f8e7649af660a904d6ac8c85c683195705ce
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-16 21:24:39 +0100

    feat: add location field to User model and update migration; seed test data with location

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
-            $table->string('address')->nullable();
+            $table->string('location');
             $table->string('team')->nullable();
             $table->integer('role')->default(0);
             $table->rememberToken();
