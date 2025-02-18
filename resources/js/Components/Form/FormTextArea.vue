f9460f5fa3f3c2a54f3c85ec3c2c20a40cfed103
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-18 13:10:16 +0100

    feat: add FormTextArea component with props for label, value, error handling, and required indication

---
+++ resources/js/Components/Form/FormTextArea.vue
@@ -0,0 +1,20 @@
+<template>
+    <label :for="name" class="mb-2 md:mb-0">{{ labelName }} <span v-if="required" class="required-star-style">*</span></label>
+    <textarea :type="type" :name="name" :value="modelValue" 
+        @input="$emit('update:modelValue', $event.target.value)" class="input-style">
+    </textarea>
+    <div v-if="error" class="input-error-message-style">{{ error }}</div>
+</template>
+
+<script setup>
+defineProps({
+    name: String,
+    labelName: String,
+    type: String,
+    modelValue: String,
+    error: String,
+    required: Boolean
+})
+
+defineEmits(["update:modelValue"]);
+</script>
