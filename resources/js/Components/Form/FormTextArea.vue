19a99f3a55c74c1a14ebc3ba83e9567b3bdfb871
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-18 13:12:06 +0100

    feat: remove unused type prop from FormTextArea component

--- resources/js/Components/Form/FormTextArea.vue
+++ resources/js/Components/Form/FormTextArea.vue
@@ -1,6 +1,6 @@
 <template>
     <label :for="name" class="mb-2 md:mb-0">{{ labelName }} <span v-if="required" class="required-star-style">*</span></label>
-    <textarea :type="type" :name="name" :value="modelValue" 
+    <textarea :name="name" :value="modelValue" 
         @input="$emit('update:modelValue', $event.target.value)" class="input-style">
     </textarea>
     <div v-if="error" class="input-error-message-style">{{ error }}</div>
@@ -10,7 +10,6 @@
 defineProps({
     name: String,
     labelName: String,
-    type: String,
     modelValue: String,
     error: String,
     required: Boolean
