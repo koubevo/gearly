ecc0f902b3d652ed6405eab53f060cf30419dfee
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-18 13:07:21 +0100

    feat: add required fields note component with styling for required indicators

---
+++ resources/js/Components/Form/RequiredFieldsNote.vue
@@ -0,0 +1,8 @@
+<script setup>
+import SmallText from '@/Components/Text/SmallText.vue';
+</script>
+
+<template>
+    
+    <SmallText><span class="required-star-style">*</span> Required Fields</SmallText>
+</template>
