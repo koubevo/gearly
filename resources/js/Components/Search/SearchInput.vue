849cd97307f7db1fa5f9051707c6cd26f7d42b37
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-20 19:27:11 +0100

    feat: enhance search input component with v-model binding and clear functionality

--- resources/js/Components/Search/SearchInput.vue
+++ resources/js/Components/Search/SearchInput.vue
@@ -4,13 +4,27 @@
         <span class="absolute inset-y-0 left-3 flex items-center">
             <MagnifyingGlassIcon class="w-5 h-5 stroke-[2.5]" />
         </span>
-        <input type="text" placeholder="Search" class="pl-10 pr-10 input-style bg-white focus:ring-0 focus:border-black" id="search-input" name="search"/>
-        <button class="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-black" onclick="document.getElementById('search-input').value='';" >
-            &times;
-        </button>
+        <input type="text" placeholder="Search" class="pl-10 pr-10 input-style bg-white focus:ring-0 focus:border-black" id="search-input" name="search" v-model="search" required/>
     </div>
 </template>
 
 <script setup>
 import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
+import { defineProps, defineEmits, ref, watch } from 'vue';
+
+const props = defineProps({
+    modelValue: String
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const search = ref(props.modelValue);
+
+watch(search, (newValue) => {
+    emit('update:modelValue', newValue);
+});
+
+const clearSearch = () => {
+    search.value = '';
+};
 </script>
--- resources/js/Pages/Search/Index.vue
+++ resources/js/Pages/Search/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full sm:w-2/3 md:w-1/2 mx-auto">
         <form @submit.prevent="handleSubmit">
-            <SearchInput/>
+            <SearchInput v-model="searchForm.search"/>
             <PrimaryButton text="Search" class="w-full md:w-auto mt-2"/>
         </form>
         <div class="flex flex-col items-center py-6 w-full">
@@ -18,8 +18,19 @@
 import CategoryItem from '@/Components/Search/CategoryItem.vue';
 import SearchInput from '@/Components/Search/SearchInput.vue';
 import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
+import { useForm } from '@inertiajs/vue3';
 
-defineProps({
+const props = defineProps({
     categories: Array
 });
+
+const searchForm = useForm({
+    search: ''
+});
+
+const handleSubmit = () => {
+    searchForm.get(
+        route('offer.index', { search: searchForm.search })
+    )
+};
 </script>
