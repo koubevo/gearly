2a7adbb9e238f0acce5a77291a9598ffc81b6ed7
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-20 12:03:08 +0100

    feat: add image viewer styles and conditional navigation buttons

--- resources/js/Components/offer/ImageViewer.vue
+++ resources/js/Components/offer/ImageViewer.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="gallery">
       <div class="main-image">
-        <button @click="prevImage" class="nav left">‹</button>
+        <button v-if="images.length > 1" @click="prevImage" class="nav left">‹</button>
         <img :src="currentImage" alt="Offer image" />
-        <button @click="nextImage" class="nav right">›</button>
+        <button v-if="images.length > 1" @click="nextImage" class="nav right">›</button>
       </div>
   
       <div class="thumbnails">
@@ -39,67 +39,4 @@
       currentIndex.value < props.images.length - 1 ? currentIndex.value + 1 : 0;
   };
   </script>
-  
-  <style scoped>
-  .gallery {
-    text-align: center;
-  }
-  
-  .main-image {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 500px; /* TODO: responsive */
-    height: 500px;
-    aspect-ratio: 1 / 1;
-    background-color: white;
-    overflow: hidden;
-  }
-  
-  .main-image img {
-    width: 100%;
-    /*height: 100%;*/ /*TODO: */
-    object-fit: cover;
-  }
-  
-  .nav {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    background: rgba(0, 0, 0, 0.5);
-    color: white;
-    border: none;
-    font-size: 2rem;
-    cursor: pointer;
-    padding: 10px;
-  }
-  
-  .left {
-    left: 10px;
-  }
-  
-  .right {
-    right: 10px;
-  }
-  
-  .thumbnails {
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-    margin-top: 10px;
-  }
-  
-  .thumbnails img {
-    width: 80px;
-    height: 80px;
-    object-fit: cover;
-    cursor: pointer;
-    border: 2px solid transparent;
-  }
-  
-  .thumbnails img.active {
-    border-color: green;
-  }
-  </style>
-  
+
