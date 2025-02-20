5439efb913cfb54dc3e7a2b245e93710bbd6ea25
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-20 01:04:51 +0100

    feat: implement pagination for offers in index method and add Pagination component

---
+++ resources/js/Components/offer/Pagination.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex justify-center mt-14 mb-8 w-60 mx-auto gap-2">
+        <!-- TODO: add disabled state when empty -->
+        <SecondaryLink :href="previous" class="page-link" aria-label="Previous">
+             <span aria-hidden="true">&laquo;</span>
+        </SecondaryLink>
+         <!-- TODO: 3 pages -->
+          
+         <!-- TODO: add disabled state when empty -->
+         <SecondaryLink :href="next" class="page-link" aria-label="Next">
+             <span aria-hidden="true">&raquo;</span>
+         </SecondaryLink>
+    </div>
+</template>
+
+<script setup>
+import SecondaryLink from '@/Components/Buttons/SecondaryLink.vue';
+defineProps({
+    links: Array,
+    previous: String,
+    next: String
+});
+</script>
