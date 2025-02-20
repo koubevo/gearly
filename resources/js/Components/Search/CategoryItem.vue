4c2059a54fe4027e3411243bdba6bb2a66ddf37e
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-20 19:13:13 +0100

    feat: add search input component and category item for improved search functionality

---
+++ resources/js/Components/Search/CategoryItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full my-0.5 flex flex-col justify-center">
+        <Link class="text-left w-full" :href="route('offer.index', {category_id: category.id})">
+            <div class="hover:bg-gray-300 p-3 w-full flex flex-row justify-between items-center">
+                <!-- TODO: category icon -->
+                <div class="w-10 flex-shrink-0"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/172px-Major_League_Baseball_logo.svg.png" alt="" class="w-full"></div>
+                <div class="flex-1 ps-4"><NormalText>{{ category.name }}</NormalText></div>
+                <div class="flex-shrink-0 text-primary-900"><ChevronRightIcon class="w-5 h-5  stroke-[2.5]" /></div>
+            </div>    
+        </Link>
+        <Divider/>
+    </div>
+    
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import NormalText from '@/Components/Text/NormalText.vue';
+import Divider from '@/Components/Search/Divider.vue';
+import { ChevronRightIcon } from '@heroicons/vue/24/outline';
+
+defineProps({
+    category: Object
+})
+</script>
---
+++ resources/js/Components/Search/SearchInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <label for="search-input" class="mb-2 md:mb-0">Search by text</label>
+    <div class="relative w-full inline-block mt-1">
+        <span class="absolute inset-y-0 left-3 flex items-center">
+            <MagnifyingGlassIcon class="w-5 h-5 stroke-[2.5]" />
+        </span>
+        <input type="text" placeholder="Search" class="pl-10 pr-10 input-style bg-white focus:ring-0 focus:border-black" id="search-input" name="search"/>
+        <button class="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-black" onclick="document.getElementById('search-input').value='';" >
+            &times;
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
+</script>
