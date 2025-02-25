0e7e85402db827fea899bb0a54451fc704be3325
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-25 21:46:30 +0100

    feat: add placeholder text for empty chat messages in ChatSection component

--- resources/js/Components/Chat/ChatSection.vue
+++ resources/js/Components/Chat/ChatSection.vue
@@ -2,12 +2,14 @@
     <section class="overflow-y-auto h-[80vh]">
         <div class="flex flex-col gap-2 items-start">
             <Message v-for="message in messages" :key="message.id" :message="message"/>
+            <NormalText class="text-center self-center mt-10 text-primary-900" v-if="!messages.length">Start the conversation with a message!</NormalText>
         </div>
     </section>
 </template>
 
 <script setup>
 import Message from "@/Components/Chat/Message.vue";
+import NormalText from "@/Components/Text/NormalText.vue";
 
 const props = defineProps({
     seller: Object,
