2388368b24ed58ce5e878e54ae34b293dd51c7f6
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-02-28 21:31:03 +0100

    refactor: remove phone field from users table and update offer details to display seller rating

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone')->nullable();
             $table->string('location');
             $table->string('team')->nullable();
             $table->integer('role')->default(0);
--- resources/js/Components/User/OfferUserDetail.vue
+++ resources/js/Components/User/OfferUserDetail.vue
@@ -2,11 +2,10 @@
     <section class="border-s-gray-900 border-4 border-e-0 border-y-0 p-2">
         <Heading3>{{ seller.name }} <span v-if="seller.team">({{ seller.team }})</span></Heading3>
         <!-- TODO: feedback -->
-        <NormalText>Feedback</NormalText>
+        <Rating class="mb-2"/>
         <SmallText v-if="soldOffersCount > 0" class="mb-2">Already sold items: {{ soldOffersCount }}</SmallText>
         <!-- TODO: premium badge, verified badge -->
         <TinyText :text="seller.location" class="mb-0.5"/>
-        <TinyText :text="seller.phone" v-if="seller.phone"/>
     </section>
 </template>
 
@@ -14,7 +13,7 @@
 import Heading3 from '@/Components/Text/Heading3.vue'
 import TinyText from '@/Components/Text/TinyText.vue'
 import SmallText from '@/Components/Text/SmallText.vue'
-import NormalText from '@/Components/Text/NormalText.vue'
+import Rating from '@/Components/User/Rating.vue';
 defineProps({
     seller: Object,
     soldOffersCount: Number,
