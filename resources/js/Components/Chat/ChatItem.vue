a3f210133df4c31e720f7bf1bd6093ec368b2020
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-03-11 15:22:55 +0100

    feat: add last message time to chat.index

--- resources/js/Components/Chat/ChatItem.vue
+++ resources/js/Components/Chat/ChatItem.vue
@@ -4,13 +4,14 @@
             <div class="p-3 w-full flex flex-row justify-between items-center gap-2">
                 <div class="w-10 flex-shrink-0"><img :src="chat.offer.thumbnail_url" alt="Offer image" class="w-full object-cover object-center scale-150 card-image" ></div>
                 <div class="flex-1 ps-4">
-                    <div class="grid gap-0.5">
+                    <div class="grid">
                         <div class="flex gap-2 items-center">
                             <Condition :condition="chat.offer.status" :conditionNumber="chat.offer.statusNumber" :type="'status'" />
                             <BoldNormalText>{{ chat.offer.name }}</BoldNormalText>
                         </div>
                         <SmallText v-if="chat.buyer_id !== user.id">{{ chat.buyer_name }}</SmallText>
                         <SmallText v-else>{{ chat.seller_name }}</SmallText>
+                        <TinyText>{{ chat.last_message_time }}</TinyText>
                     </div>
                 </div>
                 <div class="flex-shrink-0">
@@ -33,6 +34,7 @@ import BoldNormalText from '@/Components/Text/BoldNormalText.vue';
 import PriceCard from '@/Components/Offer/PriceCard.vue';
 import SmallText from '@/Components/Text/SmallText.vue';
 import Condition from '@/Components/Offer/Condition.vue';
+import TinyText from '../Text/TinyText.vue';
 
 const user = usePage().props.auth.user ?? {};
 
