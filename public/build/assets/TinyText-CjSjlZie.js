b82ee92c3196cf34ec10589e4afffc7662da76c7
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-03-19 16:13:13 +0100

    Delete public/build/assets directory

--- public/build/assets/TinyText-CjSjlZie.js
+++
@@ -1,1 +0,0 @@
-import{o as s,d as o,g as a,t as r,p as n}from"./app-CuUtu08i.js";const c={class:"tiny-text-style"},i={__name:"TinyText",props:{text:String},setup(t){return(e,l)=>(s(),o("p",c,[a(r(t.text),1),n(e.$slots,"default")]))}};export{i as _};
