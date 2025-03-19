15e09b3436e29b16b7e2e98e239823e4c30ddc11
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-03-19 17:33:44 +0100

    fix: offer.show image gallery bug

--- resources/js/Components/Offer/ImageViewer.vue
+++ resources/js/Components/Offer/ImageViewer.vue
@@ -1,42 +1,45 @@
 <template>
-    <div class="gallery w-full mb-4">
-      <div class="main-image">
-        <button v-if="images.length > 1" @click="prevImage" class="nav left">‹</button>
-        <img :src="currentImage" alt="Offer image" />
-        <button v-if="images.length > 1" @click="nextImage" class="nav right">›</button>
-      </div>
-  
-      <div class="thumbnails">
+  <div class="gallery w-full mb-4">
+    <div class="main-image relative flex items-center justify-center mx-auto">
+      <button v-if="images.length > 1" @click="prevImage" class="nav left">‹</button>
+      <img :src="currentImage" alt="Offer image" class="max-w-full max-h-[300px] object-contain" />
+      <button v-if="images.length > 1" @click="nextImage" class="nav right">›</button>
+    </div>
+
+    <div class="thumbnails-container overflow-x-auto flex gap-2 mt-2 pl-2">
+      <div class="thumbnails flex flex-nowrap">
         <img
           v-for="(img, index) in images"
           :key="index"
           :src="img"
-          :class="{ active: index === currentIndex }"
+          :class="{ 'border-2 border-primary-900': index === currentIndex }"
+          class="h-16 object-cover cursor-pointer"
           @click="currentIndex = index"
           alt="Thumbnail"
         />
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, computed } from "vue";
-  
-  const props = defineProps({
-    images: Array
-  });
-  
-  const currentIndex = ref(0);
-  const currentImage = computed(() => props.images?.[currentIndex.value] || "");
-  
-  const prevImage = () => {
-    currentIndex.value =
-      currentIndex.value > 0 ? currentIndex.value - 1 : props.images.length - 1;
-  };
-  
-  const nextImage = () => {
-    currentIndex.value =
-      currentIndex.value < props.images.length - 1 ? currentIndex.value + 1 : 0;
-  };
-  </script>
+  </div>
+</template>
 
+
+<script setup>
+import { ref, computed } from "vue";
+
+const props = defineProps({
+  images: Array
+});
+
+const currentIndex = ref(0);
+const currentImage = computed(() => props.images?.[currentIndex.value] || "");
+
+const prevImage = () => {
+  currentIndex.value =
+    currentIndex.value > 0 ? currentIndex.value - 1 : props.images.length - 1;
+};
+
+const nextImage = () => {
+  currentIndex.value =
+    currentIndex.value < props.images.length - 1 ? currentIndex.value + 1 : 0;
+};
+</script>
