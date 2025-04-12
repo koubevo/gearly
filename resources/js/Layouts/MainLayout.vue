d64b44b945bcf9d1e3a9c90e9bf08c471b515a75
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-04-13 00:25:52 +0200

    add single help page, move help content to component

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -62,11 +62,6 @@
             </nav>
         </div>
     </header>
-    <div class="bg-primary-900 py-1 sm:hidden">
-      <button @click="openModal" class="flex items-center justify-center w-full">
-        <TinyText class="text-white text-center font-bold">{{ $t('help.do_you_need_help') }}</TinyText>
-      </button>
-    </div>
 
     <main class="container mx-auto px-2 mt-6">
         <Modal :show="helpModal" @close="closeModal">
@@ -74,79 +69,7 @@
             <div class="flex justify-between items-end mb-6">
               <Heading2>{{ $t('common.help') }}</Heading2>
             </div>        
-            <div class="mb-4">
-              <div
-                class="flex items-center justify-between cursor-pointer select-none"
-                @click="showBuy = !showBuy">
-                <Heading3>{{ $t('help.how_to_buy') }}</Heading3>
-                <ChevronDownIcon
-                  class="w-5 h-5"
-                  :class="{ 'rotate-180': showBuy }"
-                />
-              </div>
-              <div v-if="showBuy" class="mt-2">
-                <ol class="list-decimal ml-5 my-2">
-                  <li v-if="!user"><NormalText v-html="$t('help.login_first')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_buy_text_1')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_buy_text_2')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_buy_text_3_a')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_buy_text_4')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_buy_text_5')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_buy_text_6')" /></li>
-                </ol>
-                <BoldNormalText class="text-primary-900">
-                  {{ $t('help.how_to_buy_text_7') }}
-                </BoldNormalText>
-              </div>
-            </div>
-            <Divider class="my-4 md:w-full" />
-            <div class="mb-4">
-                <div class="flex items-center justify-between cursor-pointer select-none"
-                      @click="showSell = !showSell">
-                    <Heading3 class="cursor-pointer" @click="showSell = !showSell">
-                      {{ $t('help.how_to_sell') }}
-                    </Heading3>
-                    <ChevronDownIcon
-                        class="w-5 h-5"
-                        :class="{ 'rotate-180': showSell }"
-                      />
-                </div>
-              <div v-if="showSell">
-                <ol class="list-decimal ml-5 my-2">
-                  <li v-if="!user"><NormalText v-html="$t('help.login_first')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_1')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_2')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_3')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_4')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_5')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_6')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_sell_text_7')" /></li>
-                </ol>
-                <BoldNormalText class="text-primary-900">{{ $t('help.how_to_sell_text_8') }}</BoldNormalText>
-              </div>
-            </div>
-            <Divider class="my-4 md:w-full" />        
-            <div class="mb-4">
-                <div class="flex items-center justify-between cursor-pointer select-none"
-                      @click="showChat = !showChat">
-                    <Heading3 class="cursor-pointer" @click="showChat = !showChat">
-                      {{ $t('help.how_to_chat') }}
-                    </Heading3>
-                    <ChevronDownIcon
-                        class="w-5 h-5"
-                        :class="{ 'rotate-180': showChat }" />
-                </div>
-              <div v-if="showChat">
-                <ol class="list-decimal ml-5 my-2">
-                  <li v-if="!user"><NormalText v-html="$t('help.login_first')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_chat_text_1')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_chat_text_2')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_chat_text_3')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_chat_text_4')" /></li>
-                  <li><NormalText v-html="$t('help.how_to_chat_text_5')" /></li>
-                </ol>
-              </div>
-            </div>
+            <HelpContent :user="user" />
           </div>
         </Modal>
         <div v-if="flashSuccess" class="flash-message-success-style">
@@ -169,11 +92,7 @@ import SecondaryLink from '@/Components/Buttons/SecondaryLink.vue';
 import axios from 'axios';
 import Modal from '@/Components/Modal.vue';
 import Heading2 from '@/Components/Text/Heading2.vue';
-import Divider from '@/Components/Search/Divider.vue';
-import Heading3 from '@/Components/Text/Heading3.vue';
-import NormalText from '@/Components/Text/NormalText.vue';
-import BoldNormalText from '@/Components/Text/BoldNormalText.vue';
-import TinyText from '@/Components/Text/TinyText.vue';
+import HelpContent from '@/Components/Help/HelpContent.vue';
 
 const showBuy = ref(false)
 const showSell = ref(false)
