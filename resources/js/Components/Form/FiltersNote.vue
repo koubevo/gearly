941ff9c4f8ca827c279375680dfddac03f551fa7
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-04-19 16:44:57 +0200

    add question mark to FiltersNote.vue

--- resources/js/Components/Form/FiltersNote.vue
+++ resources/js/Components/Form/FiltersNote.vue
@@ -1,10 +1,12 @@
 <script setup>
 import SmallText from '@/Components/Text/SmallText.vue';
+import { QuestionMarkCircleIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
     
     <SmallText>
+        <QuestionMarkCircleIcon class="w-4 h-4 inline-block align-text-top stroke-[2]" />
         {{ $t('offer.filters_description') }}
         <span class="font-bold">
             {{ $t('offer.filters_description_bold') }}
