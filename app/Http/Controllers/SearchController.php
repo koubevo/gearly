2f2115193598211db39a0470d5aff22bd0452402
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-07-26 18:22:22 +0200

    refactor searchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,35 +5,21 @@ namespace App\Http\Controllers;
 use App\Helpers\LanguageHelper;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Services\SearchService;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+    public function __construct(protected SearchService $searchService)
+    {
+
+    }
+
     public function index(Request $request)
     {
-        $langColumn = LanguageHelper::getLangColumn();
-
-        $categories = Category::select('id', "$langColumn as name", "logo")
-            ->withCount([
-                'offers' => function ($query) {
-                    $query->active();
-                }
-            ])
-            ->orderBy('name', 'asc')
-            ->get();
-
-        $brands = Brand::withCount([
-            'offers' => function ($query) {
-                $query->active();
-            }
-        ])
-            ->having('offers_count', '>', 0)
-            ->orderBy('name', 'asc')
-            ->get();
-
         return inertia('Search/Index', [
-            'categories' => $categories,
-            'brands' => $brands,
+            'categories' => $this->searchService->getCategories(),
+            'brands' => $this->searchService->getBrands(),
         ]);
     }
 }
---
+++ app/Services/SearchService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+use App\Helpers\LanguageHelper;
+use App\Models\Category;
+use App\Models\Brand;
+use Illuminate\Support\Collection;
+
+class SearchService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Retrieve all categories with or without active offers.
+     * @return Collection
+     */
+    public function getCategories(): Collection
+    {
+        $langColumn = LanguageHelper::getLangColumn();
+
+        return Category::select('id', "$langColumn as name", "logo")
+            ->withCount([
+                'offers' => function ($query) {
+                    $query->active();
+                }
+            ])
+            ->orderBy('name', 'asc')
+            ->get();
+    }
+
+    /**
+     * Retrieve all brands with active offers.
+     * @return Collection
+     */
+    public function getBrands(): Collection
+    {
+        return Brand::withCount([
+            'offers' => function ($query) {
+                $query->active();
+            }
+        ])
+            ->having('offers_count', '>', 0)
+            ->orderBy('name', 'asc')
+            ->get();
+    }
+}
