a78017c76c3cf7fb279c8d72cebdb3681567306d
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-08-01 18:38:48 +0200

    add tests for userfactory

---
+++ tests/Unit/Factories/UserFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Factories;
+
+use PHPUnit\Framework\TestCase;
+
+class UserFactoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
