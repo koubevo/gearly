89039629bf745b00ce6047b989356b1d71375270
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-08-03 15:54:02 +0200

    add access tests for admin pages

---
+++ tests/Feature/Admin/AccessTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Tests\TestCase;
+use App\Models\User;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class AccessTest extends TestCase
+{
+
+    protected User $admin;
+    protected User $normalUser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->admin = User::factory()->create(['role' => 1]);
+        $this->normalUser = User::factory()->create(['role' => 0]);
+    }
+
+    #[DataProvider('adminPagesProvider')]
+    public function test_admin_can_access_admin_pages($url): void
+    {
+        $response = $this->actingAs($this->admin)->get($url);
+
+        $response->assertStatus(200);
+    }
+
+    #[DataProvider('adminPagesProvider')]
+    public function test_normal_user_can_not_access_admin_pages($url): void
+    {
+        $response = $this->actingAs($this->normalUser)->get($url);
+
+        $response->assertStatus(403);
+    }
+
+    #[DataProvider('adminPagesProvider')]
+    public function test_anonymous_user_can_not_access_admin_pages($url): void
+    {
+        $response = $this->get($url);
+
+        $response->assertStatus(302);
+    }
+
+    public static function adminPagesProvider()
+    {
+        return [
+            ['/admin'],
+            //['/admin/users'],
+            ['/admin/brands'],
+            ['/admin/categories'],
+            ['/admin/delivery-options'],
+            ['/admin/filters'],
+            ['/admin/filter-categories']
+        ];
+    }
+}
